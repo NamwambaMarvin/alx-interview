ebb75e121e8c5b870c55859b2bce9133a04a113d
Namwamba <marvinnamwamba@gmail.com>
2024-01-14 17:31:08 -0800

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+"""
+ Lock boxes problem AFRICA Problem
+"""
+
+def canUnloakAll(boxes):
+    """
+    Lock boxes plobem solved
+    """
+    if (type(boxes) is not list):
+        return False
+    elif (len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        checked = False
+        for idx in range(len(boxes)):
+            checked = k in boxes[idx] and k != idx
+                if checked:
+                    break
+        if checked is False:
+            return checked
+    return True
