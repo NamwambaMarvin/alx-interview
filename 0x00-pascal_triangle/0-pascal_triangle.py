49806da5d4a75ac25a0f91318e74c58327c05fb0
= <=>
2024-02-09 09:38:32 -0800

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists containing
+    pascal triangle sequence
+    """
+    mainList = []
+    if n < 0:
+        return mainList
+    mainList = [[1]]
+    if n == 1:
+        return mainList
+
+    for row in range(1, n):
+        toleft = -1  # Left
+        toright = 0  # Right
+        innerList = []
+        for column in range(row + 1):
+            integer = 0
+            if toleft > -1:
+                integer += mainList[row - 1][toleft]
+            if toright < row:
+                integer += mainList[row - 1][toright]
+            toright += 1
+            toleft += 1
+            innerList.append(integer)
+        mainList.append(innerList)
+    return mainList
