0bf0d5c720223c6cafbac15173d9156e7d53d3a2
= <=>
2024-02-18 17:52:34 -0800

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
-/* Use the requests modeule to get stars wars API */
+/* Star Wars Characters - Using the request module */
 const request = require('request');
 const urlApi = 'https://swapi-api.alx-tools.com/api/films/';
 const movieId = process.argv[2];
-
-// Get from the API
+// query API
 request(urlApi + movieId, (error, response, body) => {
-    if (error) throw error;
-    const characters = JSON.parse(body).characters;
-    showNames(characters);
+  if (error) throw error;
+  const characters = JSON.parse(body).characters;
+  showNames(characters);
 });
-// Display results
+// show results on the console
 const showNames = (names, i = 0) => {
-    if (i === names.length) return;
-    request(names[i], (error, response, body) => {
-	if (error) throw error;
-	console.log(JSON.parse(body).name);
-	showNames(names, i + 1);
-    });
+  if (i === names.length) return;
+  request(names[i], (error, response, body) => {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    showNames(names, i + 1);
+  });
 };
